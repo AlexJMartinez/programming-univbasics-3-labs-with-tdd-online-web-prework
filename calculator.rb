66aece3786aa059e32d4824ddec3192d6bd436cd
Alex Martinez <alexandr3martin3z@gmail.com>
2019-09-22 02:06:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
-first_number = 5
-second_number = 8
-sum = 13
-difference = -3
-product = 40
-quotient = 1.6
+first_number = 4
+second_number = 4
+sum = 8
+difference = 0
+product = 16
+quotient = 1
